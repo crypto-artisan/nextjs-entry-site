ea78ee2eb35e254dc7ace4d6a4dd82fae3520494
hightecular95908 <rkulkin47@gmail.com>
2024-06-22 00:06:24 +0900

    404pageUpdated

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,8 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <>
+            <h1>Oops!</h1>;
+            <h2>Page Not Found</h2>;
+        </>
+    )
 }
